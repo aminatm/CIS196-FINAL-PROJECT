306f2e917209ce2b8f582972ce457ff272e2b69a
Aminat Musa <aminatm@wharton.upenn.edu>
2016-04-18 12:06:47 -0400

    Created new rails app

---
+++ MyWeddingVendor/app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :is_vendor, :created_at, :updated_at
---
+++ MyWeddingVendor/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyWeddingVendor::Application.config.secret_key_base = '0bee05997373c1bc36aa58f00832fe860af18df1f0c263617e9924c8b507eae4671b05b9fdf86ad7daf9cf401e389c83e53ff1487a7e5818b5b58cb349d3c3c4'
